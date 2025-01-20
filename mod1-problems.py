ef6110005daf0fe177a346149de81f5eae224769
James Duffy <jamesduffy@student.olympic.edu>
2025-01-20 03:59:13 -0800

    Update mod1-problems.py to increase whitespace before submission

--- mod1-problems.py
+++ mod1-problems.py
@@ -1,6 +1,7 @@
 import math
 # 1. Create a program that prints the following output to the screen: "Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked."
 print("Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked.")
+
 # 2. Create a program that prompts for 2 numbers and then outputs the addition, subtraction, multiplication, and division of the first number by the second number.
 num_one = float(input("Please provide a number: "))
 num_two = float(input("Please provide a second number: "))#asks nicely, then produces results
@@ -8,20 +9,26 @@ num_add = float(num_one + num_two)
 num_sub = float(num_one - num_two)
 num_mult = float(num_one * num_two)
 num_div = float(num_one / num_two)
+
 print(f"Here are your two numbers Added - {num_add}, Subtracted - {num_sub}, Multiplied - {num_mult}, Divided - {num_div}")
+
 # 3. Create a program that prompts for the side lengths of a triangle and computes the area using Heron's formula.
 a = float(input("Enter the side length of side A: "))
 b = float(input("Enter the side length of side B: "))
 c = float(input("Enter the side length of side C: ")) #first, ask for all sides, then calcs semi-perimeter:
+
 s = (a+b+c)/2
 area = math.sqrt(s*(s-a)*(s-b)*(s-c)) #then we Heron it:
+
 print(f"The area of your very fun triangle is {area:.2f} units")
+
 # 4. Create a program that computes different statistics given five numbers including the total, average, minimum, maximum, range, and standard deviation
 num1 = float(input("Next, I'll need five more numbers from you, First number? "))
 num2 = float(input("Second Number? "))
 num3 = float(input("Third Number? "))
 num4 = float(input("Fourth Number? "))
 num5 = float(input("Fifth Number? "))
+
 total = (num1+num2+num3+num4+num5) #start with adding them all together for the total
 average = total/5 #easy average with five numbers
 minimum = min(num1,num2,num3,num4,num5) #finding the minimum
@@ -33,6 +40,7 @@ variance = (((num1 - average) ** 2) + #first we find the mean and subtract it fr
     ((num4 - average) ** 2) +           #square root.
     ((num5 - average) ** 2)) / 5
 std_deviation = math.sqrt(variance)
+
 print(f"Your Total is: {total:.2f}")
 print(f"Average: {average:.2f}")
 print(f"Minimum: {minimum:.2f}")
